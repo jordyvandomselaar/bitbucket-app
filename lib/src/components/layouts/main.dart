02e5e8efbd6496d3529e60243c80e9682f126f64
jordy <jordy.van.domselaar@outlook.com>
2018-03-03 13:14:05 +0100

    Add bottom navigation.

--- lib/src/components/layouts/main.dart
+++ lib/src/components/layouts/main.dart
@@ -16,27 +16,24 @@ class Main extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Bucketeer"),
       ),
-      drawer: new Container(
-        color: Theme
-            .of(context)
-            .backgroundColor,
-        width: mediaQuery.size.width / 2,
-        child: new ListView(
-          children: <Widget>[
-            new ListTile(
-                title: new Text("Home"),
-                onTap: () => Navigator.pushNamed(context, "/")
-            ),
-            new ListTile(
-                title: new Text("Repositories"),
-                onTap: () => Navigator.pushNamed(context, "/repositories")
-            )
-          ],
-        ),
-      ),
       body: new Container(
         child: this.body,
         padding: const EdgeInsets.all(10.0),
+      ),
+
+      bottomNavigationBar: new ButtonBar(
+        children: <Widget>[
+          new FlatButton.icon(
+            icon: new Icon(Icons.home),
+            onPressed: () => Navigator.pushNamed(context, "/"),
+            label: new Text("Home"),
+          ),
+          new FlatButton.icon(
+              onPressed: () => Navigator.pushNamed(context, "/repositories"),
+              icon: new Icon(Icons.list),
+              label: new Text("Repositories")
+          )
+        ],
       ),
     );
   }
